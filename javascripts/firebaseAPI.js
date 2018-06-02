a905dd330c9b78c36a59c20c2dd79b25270d7340
Corey Lutrick <coreylutrick@gmail.com>
2018-06-02 10:14:08 -0700

    can delete saved movies from database now

--- javascripts/firebaseAPI.js
+++ javascripts/firebaseAPI.js
@@ -54,9 +54,30 @@ const getAllMovies = () =>
   });
 };
 
+const deleteMovieFromDb = (movieId) =>
+{
+  return new Promise ((resolve, reject) =>
+  {
+    $.ajax(
+      {
+        method: 'DELETE',
+        url: `${fireBaseConfig.databaseURL}/movies/${movieId}.json`,
+      })
+      .done(() =>
+      {
+        resolve();
+      })
+      .fail ((err) =>
+      {
+        reject(err);
+      });
+  });
+};
+
 module.exports =
 {
   saveMovieToWishlist,
   setConfig,
   getAllMovies,
+  deleteMovieFromDb,
 };
